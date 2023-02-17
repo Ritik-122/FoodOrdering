c8c709914f145ab56f5ddce376cd5195f9125463
ritik sharma <bharadwajritik1@gmail.com>
2023-02-18 02:12:55 +0530

    "Form Added"

--- src/components/Meal/MealItem/MealItem.js
+++ src/components/Meal/MealItem/MealItem.js
@@ -1,8 +1,9 @@
 import React from "react";
 import classes from "../MealItem/MealItem.module.css";
+import MealItemForm from "../MealItem/MealItemForm.js";
 
 export default function MealItem(props) {
-    const price=`$${props.price.toFixed(2)}`
+  const price = `$${props.price.toFixed(2)}`;
   return (
     <li className={classes.meal}>
       <div>
@@ -11,7 +12,7 @@ export default function MealItem(props) {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        
+        <MealItemForm />
       </div>
     </li>
   );
