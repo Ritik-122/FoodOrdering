8bf0c822b8bda5f345a27268f867b25ceb154efe
ritik sharma <bharadwajritik1@gmail.com>
2023-02-18 03:48:12 +0530

    "cart added"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Meals from './components/Meal/Meals.js';
+import Cart from './components/Cart/Cart.js';
 
 import Header from './components/Layout/Header.js';
+
 function App() {
   return (
     <React.Fragment>
-    
+    <Cart />
     <Header />
     <main>
     <Meals />
